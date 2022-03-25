7726b28976d534fd343add9b3d19893718f60ec2
Max Bachhuber <max.bachhuber@bahuma.io>
2022-03-25 15:43:59 +0100

    Improve newsletter

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -4,19 +4,22 @@ import Button from "react-bootstrap/Button";
 import {withFormik} from 'formik';
 import * as Yup from 'yup';
 import '../assets/styles/newsletter.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faArrowRight, faArrowDown} from '@fortawesome/free-solid-svg-icons'
 
 
 class Newsletter extends React.Component {
-
     state = {
         subscriberCount: 0,
+        showPrivacy: false,
     }
 
-    constructor(props) {
-        super(props);
+    constructor(props, context, state) {
+        super(props, context);
+
+        this.showPrivacy = this.showPrivacy.bind(this);
     }
+
 
     componentDidMount() {
         fetch('https://backend.startklar.bayern/api/newsletter')
@@ -29,7 +32,6 @@ class Newsletter extends React.Component {
 
     render() {
         let {
-            isValid,
             touched,
             handleChange,
             handleSubmit,
@@ -37,7 +39,6 @@ class Newsletter extends React.Component {
             values,
             errors,
             isSubmitting,
-            setFieldTouched,
             status,
         } = this.props;
 
@@ -52,8 +53,9 @@ class Newsletter extends React.Component {
             <div className="newsletter">
                 <div className="text-center">
                     <h6>Bleib immer auf dem Laufenden</h6>
-                    <p>Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed diam nonumy eirmod empor ividunt ut
-                    labore et dolore magna aliquyam. </p>
+                    <p>Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed diam nonumy eirmod empor ividunt
+                        ut
+                        labore et dolore magna aliquyam. </p>
                 </div>
 
                 {isSubmitting &&
@@ -62,77 +64,80 @@ class Newsletter extends React.Component {
                 {!isSubmitting && status.error &&
                     <Alert variant="danger">
                         <p style={{marginBottom: 0}}>
-                            Ein Fehler ist aufgetreten. Bitte wende dich an <a href="mailto:info@startklar.bayern">info@startklar.bayern</a>
+                            Ein Fehler ist aufgetreten. Bitte wende dich an <a
+                            href="mailto:info@startklar.bayern">info@startklar.bayern</a>
                         </p>
                     </Alert>}
 
                 {!isSubmitting && status.success &&
                     <Alert variant="success">
                         <p style={{marginBottom: 0}}>
-                            Bitte klicke auf den Bestätigungslink in der E-Mail, die wir dir gesendet haben, um die Anmeldung abzuschließen.
+                            Bitte klicke auf den Bestätigungslink in der E-Mail, die wir dir gesendet haben, um die
+                            Anmeldung abzuschließen.
                         </p>
                     </Alert>}
 
                 {!isSubmitting && !status.success && !status.error &&
-                <Form noValidate onSubmit={handleSubmit} className="mb-4">
-                    <Form.Group>
-                        <InputGroup hasValidation>
-                            <Form.Control
-                                type="email"
-                                placeholder="E-Mail Adresse"
-                                name="email"
-                                value={values.email}
-                                onChange={e => {
-                                    handleChange(e);
-                                    setFieldTouched('email');
-                                }}
-                                onBlur={handleBlur}
-                                isInvalid={!!errors.email}/>
-
-                            <Button
-                                type="submit"
-                                disabled={!isValid}
-                            >Anmelden <FontAwesomeIcon icon={faArrowRight} /></Button>
-
-                            <Form.Control.Feedback type="invalid">
-                                {errors.email}
-                            </Form.Control.Feedback>
-                        </InputGroup>
-                    </Form.Group>
-
-                    <Collapse in={touched.email}>
+                    <Form noValidate onSubmit={handleSubmit} className="mb-4">
                         <Form.Group>
-                            <Form.Check
-                                required
-                                type="checkbox"
-                                name="privacyAccepted"
-                                label="Ich habe die <a href=''>Datenschutzerklärung</a> gelesen und akzeptiere sie"
-                                id="privacyAccepted"
-                                key="checkbox-privacyAccepted"
-                            >
-                                <Form.Check.Input
-                                    type="checkbox"
-                                    isInvalid={!!touched.privacyAccepted && !!errors.privacyAccepted}
+                            <InputGroup hasValidation>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="E-Mail Adresse"
+                                    name="email"
+                                    value={values.email}
                                     onChange={handleChange}
                                     onBlur={handleBlur}
-                                />
-                                <Form.Check.Label>
-                                    Ich habe die <a href="">Datenschutzerklärung</a> gelesen und akzeptiere sie
-                                </Form.Check.Label>
+                                    onFocus={this.showPrivacy}
+                                    isInvalid={!!errors.email}/>
+
+                                <Button type="submit">Anmelden <FontAwesomeIcon icon={faArrowRight}/></Button>
 
                                 <Form.Control.Feedback type="invalid">
-                                    {errors.privacyAccepted}
+                                    {errors.email}
                                 </Form.Control.Feedback>
-                            </Form.Check>
+                            </InputGroup>
                         </Form.Group>
-                    </Collapse>
-                </Form>}
 
-                <p className="text-center"><span className={status.success ? "animation-bounce" : ''}>{status.success ? this.state.subscriberCount + 1 : this.state.subscriberCount}</span> Personen sind schon STARTKLAR!</p>
+                        <Collapse in={this.state.showPrivacy}>
+                            <Form.Group>
+                                <Form.Check
+                                    required
+                                    type="checkbox"
+                                    name="privacyAccepted"
+                                    label="Ich habe die <a href=''>Datenschutzerklärung</a> gelesen und akzeptiere sie"
+                                    id="privacyAccepted"
+                                    key="checkbox-privacyAccepted"
+                                >
+                                    <Form.Check.Input
+                                        type="checkbox"
+                                        isInvalid={!!touched.privacyAccepted && !!errors.privacyAccepted}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                    <Form.Check.Label>
+                                        Ich habe die <a href="">Datenschutzerklärung</a> gelesen und akzeptiere sie
+                                    </Form.Check.Label>
 
-                <div class="text-center"><FontAwesomeIcon icon={faArrowDown} /></div>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.privacyAccepted}
+                                    </Form.Control.Feedback>
+                                </Form.Check>
+                            </Form.Group>
+                        </Collapse>
+                    </Form>}
+
+                <p className="text-center"><span
+                    className={status.success ? "animation-bounce" : ''}>{status.success ? this.state.subscriberCount + 1 : this.state.subscriberCount}</span> Personen
+                    sind schon STARTKLAR!</p>
+
+                <div class="text-center"><FontAwesomeIcon icon={faArrowDown}/></div>
             </div>
         );
+    }
+
+    showPrivacy(e) {
+        this.setState({showPrivacy: true});
     }
 }
 
