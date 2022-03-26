58dcba288621d27ff5fc746b960d7f9c405d7b1f
Max Bachhuber <max.bachhuber@bahuma.io>
2022-03-26 21:07:58 +0100

    Add width and height, so that the browser knows the aspect ratio and can improve loading and prevent layout shift

--- src/components/sharepics.js
+++ src/components/sharepics.js
@@ -2,30 +2,44 @@ import React from 'react'
 import './../assets/styles/sharepics.scss';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faInstagram, faFacebook} from '@fortawesome/free-brands-svg-icons'
 import ShareButton from "./shareButton";
 
 const Sharepics = ({sharepics}) => {
     return (
         <div>
             <center><h2>Spread the word</h2></center>
-            <p className="text-light text-center">Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed diam nonumy eirmod empor ividunt ut labore et dolore magna aliquyam.</p>
+            <p className="text-light text-center">Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed diam
+                nonumy eirmod empor ividunt ut labore et dolore magna aliquyam.</p>
             <Row className="mb-4">
                 {sharepics.map((sharepic) => (
                     <Col xs={6} lg={3} key={'sharepic-' + sharepic.id}>
-                        <img src={sharepic.imagePreviewUrl} alt={sharepic.altText} />
+                        <img src={sharepic.imagePreviewUrl} alt={sharepic.altText} width="200" height="200"/>
                         <ShareButton
                             image={sharepic.imageShareUrl}
                             previewImage={sharepic.imagePreviewUrl}
+                            title={sharepic.title}
                             text={sharepic.body}
-                            title="Ich bin Startklar!"
                             id={sharepic.id}
-                            altText={sharepic.altText} />
+                            altText={sharepic.altText}/>
                     </Col>
                 ))}
             </Row>
-            <h6 className="follow-us text-center">Folge uns Auf: <a href="https://www.instagram.com/startklar.bayern" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a><a rel="noreferrer" href="https://www.facebook.com/startklar.bayern" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a></h6>
+            <h6 className="follow-us text-center">
+                Folge uns Auf: <a
+                href="https://www.instagram.com/startklar.bayern"
+                rel="noreferrer"
+                target="_blank">
+                <FontAwesomeIcon icon={faInstagram}/>
+            </a>
+                <a
+                    rel="noreferrer"
+                    href="https://www.facebook.com/startklar.bayern"
+                    target="_blank">
+                    <FontAwesomeIcon icon={faFacebook}/>
+                </a>
+            </h6>
         </div>
     )
 };
