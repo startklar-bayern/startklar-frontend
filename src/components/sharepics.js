becd3819c9501f9cb31bb6fdc1279d60271ca8d8
Max Bachhuber <max.bachhuber@bahuma.io>
2022-03-26 21:11:48 +0100

    Track clicks on social media icons

--- src/components/sharepics.js
+++ src/components/sharepics.js
@@ -1,47 +1,74 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './../assets/styles/sharepics.scss';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faInstagram, faFacebook} from '@fortawesome/free-brands-svg-icons'
 import ShareButton from "./shareButton";
+import ReactGA from "react-ga4";
 
-const Sharepics = ({sharepics}) => {
-    return (
-        <div>
-            <center><h2>Spread the word</h2></center>
-            <p className="text-light text-center">Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed diam
-                nonumy eirmod empor ividunt ut labore et dolore magna aliquyam.</p>
-            <Row className="mb-4">
-                {sharepics.map((sharepic) => (
-                    <Col xs={6} lg={3} key={'sharepic-' + sharepic.id}>
-                        <img src={sharepic.imagePreviewUrl} alt={sharepic.altText} width="200" height="200"/>
-                        <ShareButton
-                            image={sharepic.imageShareUrl}
-                            previewImage={sharepic.imagePreviewUrl}
-                            title={sharepic.title}
-                            text={sharepic.body}
-                            id={sharepic.id}
-                            altText={sharepic.altText}/>
-                    </Col>
-                ))}
-            </Row>
-            <h6 className="follow-us text-center">
-                Folge uns Auf: <a
-                href="https://www.instagram.com/startklar.bayern"
-                rel="noreferrer"
-                target="_blank">
-                <FontAwesomeIcon icon={faInstagram}/>
-            </a>
-                <a
-                    rel="noreferrer"
-                    href="https://www.facebook.com/startklar.bayern"
-                    target="_blank">
-                    <FontAwesomeIcon icon={faFacebook}/>
-                </a>
-            </h6>
-        </div>
-    )
-};
+export default class Sharepics extends Component {
 
-export default Sharepics
+    constructor(props, context) {
+        super(props, context);
+
+        this.trackSocialMediaClick = this.trackSocialMediaClick.bind(this);
+    }
+
+    render() {
+        const sharepics = this.props.sharepics;
+
+        return (
+            <div>
+                <center><h2>Spread the word</h2></center>
+                <p className="text-light text-center">Lorem Ipsum dolor sit amet et consetutor, sadipscing elitr, sed
+                    diam nonumy eirmod empor ividunt ut labore et dolore magna aliquyam.</p>
+
+                <Row className="mb-4">
+                    {sharepics.map((sharepic) => (
+                        <Col xs={6} lg={3} key={'sharepic-' + sharepic.id}>
+                            <img src={sharepic.imagePreviewUrl} alt={sharepic.altText} width="200" height="200"/>
+                            <ShareButton
+                                image={sharepic.imageShareUrl}
+                                previewImage={sharepic.imagePreviewUrl}
+                                title={sharepic.title}
+                                text={sharepic.body}
+                                id={sharepic.id}
+                                altText={sharepic.altText}/>
+                        </Col>
+                    ))}
+                </Row>
+
+                <h6 className="follow-us text-center">
+                    Folge uns Auf:
+                    <a
+                        href="https://www.instagram.com/startklar.bayern"
+                        rel="noreferrer"
+                        target="_blank"
+                        title="Instagram"
+                        onClick={this.trackSocialMediaClick}>
+                        <FontAwesomeIcon icon={faInstagram}/>
+                    </a>
+                    <a
+                        rel="noreferrer"
+                        href="https://www.facebook.com/startklar.bayern"
+                        target="_blank"
+                        title="Facebook"
+                        onClick={this.trackSocialMediaClick}>
+                        <FontAwesomeIcon icon={faFacebook}/>
+                    </a>
+                </h6>
+            </div>
+        );
+    }
+
+    trackSocialMediaClick(e) {
+        const platform = e.target.title;
+
+        ReactGA.event({
+            category: "socialMedia",
+            action: "clickSocialMediaLink",
+            label: platform,
+        });
+    }
+}
