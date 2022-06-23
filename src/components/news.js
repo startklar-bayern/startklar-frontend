5bbe7e82dfbd5459e8f871b32c25eb46a8c351b1
Daniel Heuberger <daniel.heuberger@whyex.com>
2022-06-23 15:31:13 +0200

    news popup bug

--- src/components/news.js
+++ src/components/news.js
@@ -7,17 +7,25 @@ import {Modal} from "react-bootstrap";
 import './../assets/styles/news.scss';
 
 export default class News extends Component {
-    state = {
-        loading: false,
-        showCopyNote: false,
-    }
-
     constructor(props, context) {
         super(props, context);
 
-        this.handleOnClick = this.handleOnClick.bind(this);
-        this.handleModalOpen = this.handleModalOpen.bind(this);
-        this.handleModalClose = this.handleModalClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+
+        this.state = {
+          show: null
+        };
+    }
+
+    handleClose() {
+        this.setState({
+            show: null
+        });
+    }
+
+    handleShow(id) {
+        this.setState({show: id});
     }
 
     render() {
@@ -36,53 +44,34 @@ export default class News extends Component {
                                 </div>
                                 <h6>{singleNews.title}</h6>
                                 <p className="text-light">{singleNews.teaser}</p>
-                                <button className="btn btn-link" onClick={this.handleOnClick}><strong>Zum Beitrag <FontAwesomeIcon icon={faArrowRight}/></strong></button>
+                                <button className="btn btn-link" onClick={() => this.handleShow(singleNews.id)}><strong>Zum Beitrag <FontAwesomeIcon icon={faArrowRight}/></strong></button>
+                                <Modal show={this.state.show === singleNews.id} onHide={this.handleClose}>
+                                    <Modal.Header>
+                                        <Modal.Title>{singleNews.title}</Modal.Title>
+                                        <button className="btn btn-link modal-close" onClick={this.handleClose}><FontAwesomeIcon icon={faXmark}/></button>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        <div className="position-relative mb-4">
+                                            <img className="newsPic" src={singleNews.previewImage.previewUrl} alt={singleNews.previewImage.altText} width={singleNews.previewImage.width} height={singleNews.previewImage.height} loading="lazy"/>
+                                            <div className="createdBadge text-light">{format(new Date(singleNews.created), "dd.MM.yyyy")}</div>
+                                        </div>
+                                        <p dangerouslySetInnerHTML={{__html: singleNews.body}} />
+                                        <Row className="additionalImages g-4">
+                                            {singleNews.images && singleNews.images.map((additionalImage) => (
+                                                <Col xs={12} lg={6}>
+                                                    <img className="additionalImage" src={additionalImage.previewUrl} alt={additionalImage.altText} width={additionalImage.width} height={additionalImage.height} loading="lazy"/>
+                                                </Col>
+                                            ))}
+                                        </Row>
+                                    </Modal.Body>
+                                </Modal>
                             </Col>
-                            <Modal show={this.state.modalOpen} onHide={this.handleModalClose}>
-                                <Modal.Header>
-                                    <Modal.Title>{singleNews.title}</Modal.Title>
-                                    <button className="btn btn-link modal-close" onClick={this.handleModalClose}><FontAwesomeIcon icon={faXmark}/></button>
-                                </Modal.Header>
-                                <Modal.Body>
-                                    <div className="position-relative mb-4">
-                                        <img className="newsPic" src={singleNews.previewImage.previewUrl} alt={singleNews.previewImage.altText} width={singleNews.previewImage.width} height={singleNews.previewImage.height} loading="lazy"/>
-                                        <div className="createdBadge text-light">{format(new Date(singleNews.created), "dd.MM.yyyy")}</div>
-                                    </div>
-                                    <p dangerouslySetInnerHTML={{__html: singleNews.body}} />
-                                    <Row className="additionalImages g-4">
-                                        {singleNews.images && singleNews.images.map((additionalImage) => (
-                                            <Col xs={12} lg={6}>
-                                                <img className="additionalImage" src={additionalImage.previewUrl} alt={additionalImage.altText} width={additionalImage.width} height={additionalImage.height} loading="lazy"/>
-                                            </Col>
-                                        ))}
-                                    </Row>
-                                </Modal.Body>
-                            </Modal>
+
                         </>
                     ))}
                 </Row>
             </div>
         );
-    }
-
-    handleOnClick(e) {
-        this.handleModalOpen();
-
-        this.setState({
-            loading: true,
-        });
-    }
-
-    handleModalOpen() {
-        this.setState({
-            modalOpen: true,
-        });
-    }
-
-    handleModalClose() {
-        this.setState({
-            modalOpen: false
-        });
     }
 }
 
