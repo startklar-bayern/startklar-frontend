90e0503635df78cc1f1886a857d235def144306c
Max Bachhuber <max.bachhuber@bahuma.io>
2022-10-15 17:24:32 +0200

    Improve styling of anmeldungAuswahl

--- src/pages/registration/anmeldungAuswahl.js
+++ src/pages/registration/anmeldungAuswahl.js
@@ -18,33 +18,32 @@ export default class AnmeldungAuswahl extends React.Component {
                   <h1  className="text-center">Anmeldung</h1>
                 </Col>
               </Row>
-              <Row className="mb-4">
-                <Col className="text-center ">
-                  <a href="/anmeldung-info">
-                    <Card>
-                      <FontAwesomeIcon icon={faPeopleGroup} size="6x" className="text-dark my-4" />
-                      <h2>Als Teilnehmer*in</h2>
+              <Row className="mb-4 align-items-stretch">
+                <Col className="text-center d-flex mb-4 mb-sm-0">
+                    <Card className="bg-black border-light p-4 align-self-stretch flex-fill" >
+                        <a href="/anmeldung-info">
+                            <FontAwesomeIcon icon={faPeopleGroup} size="6x" className="text-white my-4" />
+                            <h2>Als Teilnehmer*in</h2>
+                        </a>
                     </Card>
-                  </a>
                 </Col>
                 <Col className="text-center">
-                  <a href="/">
-                    <Card>
-                      <FontAwesomeIcon icon={faHammer} size="6x" className="text-dark my-4" />
+                    <Card className="bg-black border-light p-4 opacity-25">
+                      <FontAwesomeIcon icon={faHammer} size="6x" className="text-white my-4" />
                       <h2 >Als Helfer*in</h2>
+                        <p>Wird später freigeschaltet</p>
                     </Card>
-                  </a>
                 </Col>
               </Row>
-              <Row className="mb-4">
-                <Col>
-                  <a href="/">
-                    <Card>
-                      <h3 className="text-center">Schon angemeldet? Deine Anmeldung verwalten</h3>
-                    </Card>
-                  </a>
-                </Col>
-              </Row>
+              {/*<Row className="mb-4">*/}
+              {/*  <Col>*/}
+              {/*    <a href="/">*/}
+              {/*      <Card>*/}
+              {/*        <h3 className="text-center">Schon angemeldet? Deine Anmeldung verwalten</h3>*/}
+              {/*      </Card>*/}
+              {/*    </a>*/}
+              {/*  </Col>*/}
+              {/*</Row>*/}
             </Container>
         </div>
     )
